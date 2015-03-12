9cc8d3481eff7397d6427db7961990caeeae827b
Cody Greene <cody-greene@users.noreply.github.com>
2015-03-12 15:14:06 -0700

    add env vars to reload-client

--- reload-client.js
+++ reload-client.js
@@ -1,2 +1,3 @@
-new window.EventSource('http://localhost:9093')
+var SERVER_PORT = process.env.RELOAD_PORT || process.env.npm_package_config_reload_port || 9093
+new window.EventSource('http://localhost:' + SERVER_PORT)
   .addEventListener('reload', location.reload.bind(location, true))
